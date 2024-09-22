ee1bca1708e6c843b8a1e88b1accd54140c3e06f
Robério Filho <antonioroberiofilho8@gmail.com>
2024-09-22 20:01:56 -0300

    (chore): production config

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,13 @@ import knex, { type Knex } from "knex"; // changed the import
 import { env } from "./env";
 
 export const config: Knex.Config = {
-  client: "sqlite",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   migrations: {
     extension: "ts",
     directory: "./db/migrations",
--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === "test") {
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   DATABASE_URL: z.string(),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
 });
 
 const treatment = envSchema.safeParse(process.env);
